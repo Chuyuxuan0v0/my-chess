47efb029adf1c0b4bae73901eaa2105658b383fe
chuyxuan_mbp <mail@chuyuxuan.top>
2026-01-06 14:31:07 +0800

    feat 修改打包路径

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // 关键配置：设置为相对路径，这样无论部署在哪个子路径下都能正常访问
+  base: './',
+});
